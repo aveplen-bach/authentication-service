c80ead3fc8ae813b7a71983725566887f94a79ee
Alexey Plenkin <aveplen@yandex.ru>
2022-06-04 14:02:45 +0300

    refactor login endpoint

--- internal/controller/login.go
+++ internal/controller/login.go
@@ -11,12 +11,20 @@ import (
 func LoginUser(ls *service.LoginService) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		req := &model.LoginRequest{}
-		c.BindJSON(req)
+		if err := c.BindJSON(req); err != nil {
+			c.JSON(http.StatusNotFound, gin.H{
+				"err": err.Error(),
+			})
+			return
+		}
 
 		res, err := ls.Login(req)
 
 		if err != nil {
-			c.AbortWithError(http.StatusInternalServerError, err)
+			c.JSON(http.StatusNotFound, gin.H{
+				"err": err.Error(),
+			})
+			return
 		}
 
 		c.JSON(http.StatusOK, res)
