7de9bb67a8ea1ece3965625b27a4aedc5f510f37
Alexey Plenkin <aveplen@yandex.ru>
2022-06-04 14:10:12 +0300

    refactor register controller

--- internal/controller/register.go
+++ internal/controller/register.go
@@ -1,12 +1,35 @@
 package controller
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.com/aveplen-bach/authentication-service/internal/model"
 	"github.com/aveplen-bach/authentication-service/internal/service"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterUser(rs *service.RegisterService) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.SetCookie("jwt_token", "hello, I'm jwt-token", 3600, "/", "localhost", false, true)
+		req := &model.RegisterRequest{}
+		if err := c.BindJSON(req); err != nil {
+			c.JSON(http.StatusNotFound, gin.H{
+				"err": err.Error(),
+			})
+			return
+		}
+
+		err := rs.Register(req)
+
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{
+				"err": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"info": fmt.Sprintf("user <%s> registered successfully", req.Username),
+		})
 	}
 }
