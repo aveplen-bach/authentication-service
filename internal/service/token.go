dcde51a0c4c6556a2e17947a34a67915bca32ae0
Alexey Plenkin <aveplen@yandex.ru>
2022-06-04 17:03:15 +0300

    refactor token service

--- internal/service/token.go
+++ internal/service/token.go
@@ -17,234 +17,252 @@ type TokenService struct {
 	ss *SessionService
 }
 
-func NewTokenService() *TokenService {
-	return &TokenService{}
+func NewTokenService(ss *SessionService) *TokenService {
+	return &TokenService{
+		ss: ss,
+	}
 }
 
 func (t *TokenService) GenerateToken(userID uint) (string, error) {
-	return t.constructToken(userID)
+	raw, err := construct(userID)
+	if err != nil {
+		return "", fmt.Errorf("could not construct token: %w", err)
+	}
+
+	s, err := t.ss.Get(userID)
+	if err != nil {
+		return "", fmt.Errorf("could not get session: %w", err)
+	}
+
+	protected, err := protect(raw, s.SessionKey, s.IV)
+	if err != nil {
+		return "", fmt.Errorf("could not protect token: %w", err)
+	}
+
+	packed, err := pack(protected)
+	if err != nil {
+		return "", fmt.Errorf("could not pack token: %w", err)
+	}
+
+	return packed, nil
 }
 
 func (t *TokenService) NextToken(token string) (string, error) {
-	deconstructed, err := t.deconstructToken(token)
+	protected, err := unpack(token)
 	if err != nil {
-		return "", fmt.Errorf("could not deconstruct token: %w", err)
+		return "", fmt.Errorf("could not unpack token: %w", err)
 	}
 
-	deconstructed.Synchronization.Syn += deconstructed.Synchronization.Inc
-
-	reconstructed, err := t.constructToken(uint(deconstructed.Payload.UserID))
+	s, err := t.ss.Get(uint(protected.Payload.UserID))
 	if err != nil {
-		return "", fmt.Errorf("could not reconstruct token: %w", err)
+		return "", fmt.Errorf("could not get session: %w", err)
 	}
 
-	return reconstructed, nil
+	raw, err := unprotect(protected, s.SessionKey, s.IV)
+	if err != nil {
+		return "", fmt.Errorf("could not unprotect token: %w", err)
+	}
+
+	raw.Synchronization.Syn += raw.Synchronization.Inc
+
+	reprotected, err := protect(raw, s.SessionKey, s.IV)
+	if err != nil {
+		return "", fmt.Errorf("could not reprotect token: %w", err)
+	}
+
+	repacked, err := pack(reprotected)
+	if err != nil {
+		return "", fmt.Errorf("could not pack token: %w", err)
+	}
+
+	return repacked, nil
 }
 
 func (t *TokenService) ValidateToken(token string) (bool, error) {
-	deconstructed, err := t.deconstructToken(token)
+	protected, err := unpack(token)
 	if err != nil {
-		return false, fmt.Errorf("could not deconstruct token: %w", err)
+		return false, fmt.Errorf("could not unpack token: %w", err)
 	}
 
-	logrus.Warn("Validate token not implemented", deconstructed)
+	logrus.Warn("Validate token not implemented", protected)
 
 	return true, nil
 }
 
 func (t *TokenService) ExtractPayload(token string) (model.Payload, error) {
-	deconstructed, err := t.deconstructToken(token)
+	protected, err := unpack(token)
 	if err != nil {
 		return model.Payload{}, fmt.Errorf("could not deconstruct token: %w", err)
 	}
 
-	return deconstructed.Payload, nil
+	return protected.Payload, nil
 }
 
-func (t *TokenService) constructToken(userID uint) (string, error) {
-	synchronization, err := t.constructSynchronization(userID)
+func protect(raw model.TokenRaw, key, iv []byte) (model.TokenProtected, error) {
+	synb, err := json.Marshal(raw.Synchronization)
 	if err != nil {
-		return "", fmt.Errorf("could not construct token: %w", err)
+		return model.TokenProtected{}, fmt.Errorf("could not marshal syn: %w", err)
 	}
 
-	header, err := t.constructHeader(userID)
+	encsyn, err := cryptoutil.EncryptAesCbc(synb, key, iv)
 	if err != nil {
-		return "", fmt.Errorf("could not construct token: %w", err)
+		return model.TokenProtected{}, fmt.Errorf("could not encrypt syn: %w", err)
 	}
 
-	payload, err := t.constructPayload(userID)
+	return model.TokenProtected{
+		SynchronizationBytes: encsyn,
+		Header:               raw.Header,
+		Payload:              raw.Payload,
+		SignatureBytes:       raw.SignatureBytes,
+	}, nil
+}
+
+func unprotect(protected model.TokenProtected, key, iv []byte) (model.TokenRaw, error) {
+	rawSynBytes, err := cryptoutil.DecryptAesCbc(protected.SynchronizationBytes, key, iv)
 	if err != nil {
-		return "", fmt.Errorf("could not construct token: %w", err)
+		return model.TokenRaw{}, fmt.Errorf("could not decrypt syn: %w", err)
 	}
 
-	signature, err := t.constructSignature(header, payload)
-	if err != nil {
-		return "", fmt.Errorf("could not construct token: %w", err)
+	var rawSyn model.Synchronization
+	if err := json.Unmarshal(rawSynBytes, &rawSyn); err != nil {
+		return model.TokenRaw{}, fmt.Errorf("could not unmarshal syn: %w", err)
 	}
+
+	return model.TokenRaw{
+		Synchronization: rawSyn,
+		Header:          protected.Header,
+		Payload:         protected.Payload,
+		SignatureBytes:  protected.SignatureBytes,
+	}, nil
+}
+
+func pack(protected model.TokenProtected) (string, error) {
+	b64Syn := base64.StdEncoding.EncodeToString(protected.SynchronizationBytes)
+
+	headBytes, err := json.Marshal(protected.Header)
+	if err != nil {
+		return "", fmt.Errorf("could not marshal header part: %w", err)
+	}
+	b64Head := base64.StdEncoding.EncodeToString(headBytes)
+
+	pldBytes, err := json.Marshal(protected.Payload)
+	if err != nil {
+		return "", fmt.Errorf("could not marshal payload part: %w", err)
+	}
+	b64Pld := base64.StdEncoding.EncodeToString(pldBytes)
+
+	b64Sig := base64.StdEncoding.EncodeToString(protected.SignatureBytes)
 
 	return fmt.Sprintf(
 		"%s.%s.%s.%s",
-		synchronization,
-		header,
-		payload,
-		signature,
+		b64Syn,
+		b64Head,
+		b64Pld,
+		b64Sig,
 	), nil
 }
 
-func (t *TokenService) constructSynchronization(userID uint) (string, error) {
-	session, err := t.ss.Get(userID)
-	if err != nil {
-		return "", fmt.Errorf("could not get user session: %w", err)
-	}
-
-	syn := model.Synchronization{
-		Syn: 1,
-		Inc: 1,
-	}
-
-	b, err := json.Marshal(syn)
-	if err != nil {
-		return "", err
-	}
-
-	encSyn, err := cryptoutil.EncryptAesCbc(b, session.SessionKey, session.IV)
-	if err != nil {
-		return "", fmt.Errorf("could not encrypt syn part of the token: %w", err)
-	}
-
-	return base64.StdEncoding.EncodeToString(encSyn), nil
-}
-
-func (t *TokenService) constructHeader(userID uint) (string, error) {
-	head := model.Header{
-		SignatureAlg:  "HMACSHA256",
-		EncryptionAlg: "AESCBC",
-	}
-
-	b, err := json.Marshal(head)
-	if err != nil {
-		return "", nil
-	}
-
-	b64Head := base64.StdEncoding.EncodeToString(b)
-	return b64Head, nil
-}
-
-func (t *TokenService) constructPayload(userID uint) (string, error) {
-	payload := model.Payload{
-		UserID: int(userID),
-	}
-
-	b, err := json.Marshal(payload)
-	if err != nil {
-		return "", err
-	}
-
-	b64Payload := base64.StdEncoding.EncodeToString(b)
-	return b64Payload, nil
-}
-
-func (t *TokenService) constructSignature(header, payload string) (string, error) {
-	secret := "mysecret"
-	data := fmt.Sprintf("%s.%s", header, payload)
-
-	h := hmac.New(sha256.New, []byte(secret))
-	h.Write([]byte(data))
-	sha := base64.StdEncoding.EncodeToString(h.Sum(nil))
-
-	return sha, nil
-}
-
-func (t *TokenService) deconstructToken(token string) (model.TokenRaw, error) {
+func unpack(token string) (model.TokenProtected, error) {
 	tokenParts := strings.Split(token, ".")
 	if len(tokenParts) != 4 {
-		return model.TokenRaw{}, fmt.Errorf("token is damaged or of wrong format")
+		return model.TokenProtected{}, fmt.Errorf("token is damaged or of wrong format")
 	}
 
-	head, err := t.deconstructHeader(tokenParts[1])
+	syn, err := base64.StdEncoding.DecodeString(tokenParts[0])
 	if err != nil {
-		return model.TokenRaw{}, fmt.Errorf("could not deconstruct header: %w", err)
+		return model.TokenProtected{}, fmt.Errorf("could not decode syn: %w", err)
 	}
 
-	payload, err := t.deconstructPayload(tokenParts[2])
+	headb, err := base64.StdEncoding.DecodeString(tokenParts[1])
 	if err != nil {
-		return model.TokenRaw{}, fmt.Errorf("could not deconstruct payload: %w", err)
+		return model.TokenProtected{}, fmt.Errorf("could not decode header: %w", err)
 	}
 
-	syn, err := t.deconstructSynchronization(uint(payload.UserID), tokenParts[0])
-	if err != nil {
-		return model.TokenRaw{}, fmt.Errorf("could not deconstruct syn: %w", err)
+	var head model.Header
+	if err := json.Unmarshal(headb, &head); err != nil {
+		return model.TokenProtected{}, fmt.Errorf("could not unmarshal header: %w", err)
 	}
 
-	sign, err := t.deconstructSignature(tokenParts[3])
+	pldb, err := base64.StdEncoding.DecodeString(tokenParts[2])
 	if err != nil {
-		return model.TokenRaw{}, fmt.Errorf("could not deconstruct signature: %w", err)
+		return model.TokenProtected{}, fmt.Errorf("could not decode payload: %w", err)
+	}
+	var payload model.Payload
+	if err := json.Unmarshal(pldb, &payload); err != nil {
+		return model.TokenProtected{}, fmt.Errorf("could not unmarshal payload: %w", err)
+	}
+
+	sign, err := base64.StdEncoding.DecodeString(tokenParts[3])
+	if err != nil {
+		return model.TokenProtected{}, fmt.Errorf("could not decode sign: %w", err)
+	}
+
+	return model.TokenProtected{
+		SynchronizationBytes: syn,
+		Header:               head,
+		Payload:              payload,
+		SignatureBytes:       sign,
+	}, nil
+}
+
+func construct(userID uint) (model.TokenRaw, error) {
+	syn := defaultSyn()
+	head := defaultHead()
+	pld := defaultPld(userID)
+	sign, err := defaultSign(head, pld)
+	if err != nil {
+		return model.TokenRaw{}, fmt.Errorf("could not construct token: %w", err)
 	}
 
 	return model.TokenRaw{
 		Synchronization: syn,
 		Header:          head,
-		Payload:         payload,
+		Payload:         pld,
 		SignatureBytes:  sign,
 	}, nil
 }
 
-func (t *TokenService) deconstructSynchronization(userID uint, synStr string) (model.Synchronization, error) {
-	synBytes, err := base64.StdEncoding.DecodeString(synStr)
-	if err != nil {
-		return model.Synchronization{}, fmt.Errorf("could not decode syn: %w", err)
+func defaultSyn() model.Synchronization {
+	return model.Synchronization{
+		Syn: 1,
+		Inc: 1,
 	}
-
-	session, err := t.ss.Get(userID)
-	if err != nil {
-		return model.Synchronization{}, fmt.Errorf("could not get user session: %w", err)
-	}
-
-	b, err := cryptoutil.DecryptAesCbc(synBytes, session.SessionKey, session.IV)
-	if err != nil {
-		return model.Synchronization{}, fmt.Errorf("could not decrypt syn: %w", err)
-	}
-
-	var syn model.Synchronization
-	if err := json.Unmarshal(b, &syn); err != nil {
-		return model.Synchronization{}, fmt.Errorf("could not unmarshal syn: %w", err)
-	}
-
-	return syn, nil
 }
 
-func (t *TokenService) deconstructHeader(headerStr string) (model.Header, error) {
-	headBytes, err := base64.StdEncoding.DecodeString(headerStr)
-	if err != nil {
-		return model.Header{}, fmt.Errorf("could not decode header: %w", err)
+func defaultHead() model.Header {
+	return model.Header{
+		SignatureAlg:  "HMACSHA256",
+		EncryptionAlg: "AESCBC",
 	}
-
-	var head model.Header
-	if err := json.Unmarshal(headBytes, &head); err != nil {
-		return model.Header{}, fmt.Errorf("could not unmarshal header: %w", err)
-	}
-
-	return head, nil
 }
 
-func (t *TokenService) deconstructPayload(payloadStr string) (model.Payload, error) {
-	payloadBytes, err := base64.StdEncoding.DecodeString(payloadStr)
-	if err != nil {
-		return model.Payload{}, fmt.Errorf("could not decode payload: %w", err)
+func defaultPld(userID uint) model.Payload {
+	return model.Payload{
+		UserID: int(userID),
 	}
-
-	var payload model.Payload
-	if err := json.Unmarshal(payloadBytes, &payload); err != nil {
-		return model.Payload{}, fmt.Errorf("could not unmarshal header: %w", err)
-	}
-
-	return payload, nil
 }
 
-func (t *TokenService) deconstructSignature(signatureStr string) ([]byte, error) {
-	signatureBytes, err := base64.StdEncoding.DecodeString(signatureStr)
+func defaultSign(header model.Header, payload model.Payload) ([]byte, error) {
+	headb, err := json.Marshal(header)
 	if err != nil {
-		return nil, fmt.Errorf("could not decode signature: %w", err)
+		return nil, fmt.Errorf("could not marshal header: %w", err)
 	}
-	return signatureBytes, nil
+
+	pldb, err := json.Marshal(payload)
+	if err != nil {
+		return nil, fmt.Errorf("could not marshal payload: %w", err)
+	}
+
+	secret := "mysecret"
+	data := fmt.Sprintf(
+		"%s.%s",
+		base64.StdEncoding.EncodeToString(headb),
+		base64.StdEncoding.EncodeToString(pldb))
+
+	h := hmac.New(sha256.New, []byte(secret))
+	if _, err := h.Write([]byte(data)); err != nil {
+		return nil, fmt.Errorf("could not create sign: %w", err)
+	}
+
+	return h.Sum(nil), nil
 }
