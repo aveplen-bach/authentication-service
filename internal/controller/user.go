cad4d71ec234aa5a62d223026bbdff3f348807be
Alexey Plenkin <aveplen@yandex.ru>
2022-06-04 14:11:01 +0300

    refactor list users endpoint

--- internal/controller/user.go
+++ internal/controller/user.go
@@ -7,43 +7,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type UserController struct {
-	service *service.Service
-}
+func ListUsers(us *service.UserService) gin.HandlerFunc {
+	return func(c *gin.Context) {
 
-func NewUserController(service *service.Service) *UserController {
-	return &UserController{
-		service: service,
+		users, err := us.GetAllUsers()
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{
+				"err": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"info":  "users fetched successfully",
+			"users": users,
+		})
 	}
-}
-
-func (u *UserController) ListUsers(c *gin.Context) {
-	var users []gin.H = []gin.H{
-		{
-			"id":       1,
-			"username": "username1",
-			"vector":   "vector1",
-		},
-		{
-			"id":       2,
-			"username": "username2",
-			"vector":   "vector2",
-		},
-		{
-			"id":       3,
-			"username": "username3",
-			"vector":   "vector3",
-		},
-		{
-			"id":       4,
-			"username": "username4",
-			"vector":   "vector4",
-		},
-	}
-
-	// u.db.Find(&users)
-
-	c.JSON(http.StatusOK, gin.H{
-		"users": users,
-	})
 }
