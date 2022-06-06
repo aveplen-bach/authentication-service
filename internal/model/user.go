37225413d77efaa798dcfeaf9420c7aac196a2cc
Alexey Plenkin <aveplen@yandex.ru>
2022-06-06 15:54:27 +0300

    add user dto to fetch from frontend

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,10 +1,23 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
 	Username string
 	Password string
 	FFVector string
+	Admin    bool
+}
+
+type UserDto struct {
+	UserID    uint      `json:"userId"`
+	Username  string    `json:"username"`
+	Admin     bool      `json:"admin"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updateddAt"`
 }
