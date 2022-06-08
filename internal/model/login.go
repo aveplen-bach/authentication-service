4f708053a18903c185ef7b02108b77fd19ed8db9
Alexey Plenkin <aveplen@yandex.ru>
2022-06-09 01:00:45 +0300

    fix login model

--- internal/model/login.go
+++ internal/model/login.go
@@ -2,9 +2,7 @@ package model
 
 type LoginRequest struct {
 	// must be present at all client messages
-	Stage int `json:"stage"`
-
-	// must be present in first client message (client conn init stage)
+	Stage    int    `json:"stage"`
 	Username string `json:"username"`
 
 	// must be present in second client message (client cridentials stage)
